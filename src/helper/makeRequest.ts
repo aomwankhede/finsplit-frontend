643021610cfcfba44f64c77b1fe07016f98e30c9
aomwankhede <wankhede.aom@gmail.com>
2025-07-29 16:01:14 +0530

    add ngrok warning header

--- src/helper/makeRequest.ts
+++ src/helper/makeRequest.ts
@@ -8,17 +8,19 @@ const makeRequest = async (
 ): Promise<any> => {
   try {
     // const BASE_URL = 'http://localhost:8080';
-    const BASE_URL = 'https://fda0251d97e6.ngrok-free.app';
+    const BASE_URL = 'https://8a41a84d79f8.ngrok-free.app';
     const url = BASE_URL + path;
     const tok_n = await localStorage.getItem('token');
     if (tok_n) {
       header['Authorization'] = `Bearer ${tok_n}`;
+      header['ngrok-skip-browser-warning'] = '69420';
     }
     switch (reqType.toUpperCase()) {
       case 'GET': {
         const response = await axios.get(url, {
           headers: header,
         });
+        console.log(url, '*****', response);
         return response.data;
       }
       case 'POST': {
